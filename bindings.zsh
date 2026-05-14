8221bf256d4c994b05d9f5d1ab4b746aaf3ceca2
Radley E. Sidwell-Lewis <rad@sidwelltechnologies.com>
2026-05-14 19:35:10 +0700

    chore: block cursor

--- bindings.zsh
+++ bindings.zsh
@@ -2,6 +2,11 @@
 # Keybindings
 # =========================================================
 
+# Cursor shape per vi mode
+ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BEAM
+ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+ZVM_VISUAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+
 # Disable command mode line highlight
 ZVM_VI_HIGHLIGHT_BACKGROUND=none
 ZVM_VI_HIGHLIGHT_FOREGROUND=none
